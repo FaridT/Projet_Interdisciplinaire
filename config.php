5b7ab99fe3fecc8943ab8139a847ab309eaae3cd
OtmanFaouaji <otmanfaouaji@gmail.com>
2016-03-08 13:20:43 +0000

    Revert "Livrable 1"
    This reverts commit 9cc795c906bcf4ce6905df85395955a4a30c8361.

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-//On se connecte a la base de donnee
-@mysql_connect('', 'root', '')or die(mysql_error());
-mysql_select_db('projet_interdisciplinaire');
-mysql_query("SET NAMES 'utf8'");
-?>
--- new.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$ip = $_SERVER['REMOTE_ADDR'];
-$Ip = $_SERVER['SERVER_ADDR'];
-echo $ip.'<br>';
-echo $Ip.'<br>';
-echo $_SERVER['SERVER_NAME'].'<br>';
-?>
